719a34184291e97d8b7acec21df243f48d04ea55
CulturalProfessor <svinayak994@gmail.com>
2023-01-14 22:48:49 +0530

    added mongo atlas

--- index.js
+++ index.js
@@ -6,13 +6,29 @@ const pitchroute = require('./routes/pitchroute')
 const app = express()
 app.use(express.json())
 app.use(cors())
-mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://localhost/Pitchers')
-const db = mongoose.connection
-db.on('open', () => {
-    console.log('Database connected successfully!');
+// mongoose.set('strictQuery', true);
+// mongoose.connect('mongodb://localhost/Pitchers')
+// const db = mongoose.connection
+// db.on('open', () => {
+//     console.log('Database connected successfully!');
+// })
+
+
+const CONNETION_URL = "mongodb+srv://vinayak:25JUL2001@cluster0.loxbtih.mongodb.net/?retryWrites=true&w=majority";
+mongoose.set("strictQuery", false);
+mongoose
+.connect(CONNETION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+  console.log("MongoDB connected");
 })
+.catch((err) => {
+  console.log(err);
+});
+
+
 app.use('/', userroutes)
 app.use('/pitch',pitchroute)
 const PORT = process.env.port||3001
-app.listen(PORT)
+app.listen(PORT,()=>{
+    console.log(`Server running on PORT ${PORT}`);
+})
